d128b2f2cdd506c8aaa61a7e33e8c8dc4115a0b6
Anirudha Paul <anirudhaprasun@gmail.com>
2020-11-14 19:40:53 +0600

    A memory efficient alternative

--- Populating Next Right Pointers in Each Node.cpp
+++ Populating Next Right Pointers in Each Node.cpp
@@ -71,3 +71,41 @@ public:
         return root;
     }
 };
+
+
+//Memory efficient solution 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root==NULL) {
+            return root;
+        }
+        if(root->left&&root->right) {
+            root->left->next = root->right;
+        }
+        if(root->next && root->right && root->next->left) {
+            root->right->next = root->next->left;
+        }
+        connect(root->left);
+        connect(root->right);
+        return root;
+    }
+};
